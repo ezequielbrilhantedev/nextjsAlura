fd49e3b9a0559ce8d967aac6750ffda84cc82a30
Ezequiel Brilhante <ezequielrb7@gmail.com>
2023-06-02 13:22:12 -0300

    estruturando e estilizando o projeto

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
-import Link from '../src/components/Link';
+import HomeScreen from '../src/screens/HomeScreen';
 
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Alura Cases - Home Page</h1>
-      <Link href={'/faq'}>Ir para a página do faq</Link>
-    </div>
-  );
-}
+export default HomeScreen;
+
+// import Link from '../src/components/Link';
+// import Footer from '../src/components/patterns/Footer';
+
+// function Title({ children, as }) {
+//   const Tag = as;
+//   return (
+//     <>
+//       <Tag>{children}</Tag>
+//       <style jsx>{`
+//         ${Tag} {
+//           color: red;
+//         }
+//       `}</style>
+//     </>
+//   );
+// }
+
+// export default function HomePage() {
+//   return (
+//     <div>
+//       <Title as="h1">Alura Cases - Home Page</Title>
+//       <Link href={'/faq'}>Ir para a página do faq</Link>
+//       <Footer />
+//     </div>
+//   );
+// }
